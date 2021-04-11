ba7549143e2cdbc36a9b804057427cbe4666f5f0
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-04-11 09:09:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* Given Code, don't edit */
 
+
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
@@ -10,4 +11,26 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+const time = document.querySelector('#time').value;
+function greet(timeString) {
+  
+  const splitTime = timeString.split(':');
+  
+  if (splitTime[0] < 12){
+    return 'Good Morning';
+  }else if (splitTime[0] >= 12 && splitTime[0] <= 17){
+    return 'Good Afternoon';
+  }else if (splitTime[0] > 17){
+    return 'Good Evening';
+  }
+  
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(time){
+const display = document.querySelector('#greeting');
+  display.innerText = '';
+  display.innerText = time;
+  
+}
